b42d7701b785dd47db6a09194c65c3951e45c965
Raquiel Ribeiro Alexandre <ribeiro.raquiel@gmail.com>
2024-07-25 16:58:44 -0300

    [200~

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -1,14 +1,12 @@
 package aplication;
 
-import boardgame.Board;
-import boardgame.Position;
 import chess.ChessMatch;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Board board = new Board(8, 8);
+		
 	
 		ChessMatch chessMacth = new ChessMatch();
 		UI.printBoard(chessMacth.getPieces());
